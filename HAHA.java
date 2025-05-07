6665c6b8aab8dc2d29d8c7ae71ade39a522ceaa1
Abbas-F <54941534+Abbas-F@users.noreply.github.com>
2025-05-07 16:24:45 +0800

    quick update
    added a println line there.

---
+++ HAHA.java
@@ -0,0 +1,61 @@
+// public class HAHA{
+//     public static void main(String[] args){
+//         boolean result = isValidTriangle(4, 9 , 5);
+//         System.out.println(result);
+//     }
+
+//     public static boolean isValidTriangle(double a, double b, double c) {
+//         if (a+b < c || b+c < a || a+c < b )
+//         return false;
+//         else
+//         return true;
+// }
+// }
+
+// public class HAHA{
+//     public static void main(String[] arges){
+//         cake (new String[] {"Amy", "Philip", "Hubert"});
+        
+//     }
+//     public static void cake(String[] names) {
+//         int x = 0;
+//         while( x < names.length){
+//             System.out.println("Thank you "+ names[x] + ".You have ticket number " + (x+1));
+//             x+=1;       
+// } 
+//     }
+// }
+import java.util.Scanner;
+class BankAccount6 {
+String ownerName;
+int balance;
+BankAccount6(String ownerName, int balance) {
+this.ownerName = ownerName;
+this.balance = balance;
+}
+BankAccount6(String ownerName) {
+this.ownerName = ownerName;
+this.balance = 0;
+}
+}
+
+public class HAHA {
+public static void main(String[] args) {
+
+String s = "CITS2005";
+String cits = s.substring(0, 4);
+String code = s.substring(4);
+System.out.println(s);
+System.out.println(cits + " -- " + code);
+System.out.println(2 + "fads"+ 3);
+}
+}
+//sadfadsflkads         
+
+
+
+
+
+
+
+    
